6674bf78d6602ec0d3c1c4523257553570928b4c
zhengxingXue <zx252cornell@gmail.com>
2021-05-06 11:29:30 -0500

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Jim's MacBook Pro on 5/6/21.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
