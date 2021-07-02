7d27832e2939dd5b81dc1a152091c67c31bbd1e0
zhengxingXue <zx252cornell@gmail.com>
2021-07-02 12:34:38 -0500

    Lecture 14: UIKit Integration

--- EmojiArt/EmojiArtDocument.swift
+++ EmojiArt/EmojiArtDocument.swift
@@ -96,7 +96,7 @@ class EmojiArtDocument: ObservableObject, Hashable, Identifiable {
     
     private func fetchBackgroundImageData() {
         backgroundImage = nil
-        if let url = self.emojiArt.backgroundURL {
+        if let url = self.emojiArt.backgroundURL?.imageURL {
             fetchImageCancellable?.cancel()
             fetchImageCancellable = URLSession.shared.dataTaskPublisher(for: url)
                 .map { data, urlResponse in UIImage(data: data)}
